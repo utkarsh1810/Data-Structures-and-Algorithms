d976bc4847cd9fa47326f619748336df16a09da1
Utkarsh <utkarshverma782@gmail.com>
2020-08-17 22:56:30 +0530

    postOrder traversal in a binary tree

---
+++ Tree/Traversal/Postorder/Iterative.java
@@ -0,0 +1,56 @@
+import java.util.Stack;
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  Node(int item) {
+    data = item;
+    left = right = null;
+  }
+}
+
+class Iterative {
+  Node root;
+  void postOrder() {
+    if(root == null) 
+      return;
+
+    Stack<Node> stk1, stk2;
+    stk1 = new Stack<>();
+    stk2 = new Stack<>();
+
+    stk1.push(root);
+    
+    while(!stk1.isEmpty()) {
+      Node temp = stk1.pop();
+      stk2.push(temp);
+
+      if(temp.left != null) 
+        stk1.push(temp.left);
+
+      if(temp.right != null) 
+        stk1.push(temp.right);
+    }
+    while (!stk2.isEmpty()) { 
+      Node temp = stk2.pop(); 
+      System.out.println(temp.data + " "); 
+    } 
+
+
+  }
+  public static void main(String[] args) {
+    Iterative tree = new Iterative();
+    tree.root = new Node(1);
+    tree.root.left = new Node(2); 
+    tree.root.right = new Node(3); 
+    tree.root.left.left = new Node(4); 
+    tree.root.left.right = new Node(5); 
+    tree.root.right.left = new Node(6); 
+    tree.root.right.right = new Node(7); 
+
+    tree.postOrder(); 
+
+  }
+}
